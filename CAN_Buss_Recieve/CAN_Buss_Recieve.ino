9be3811718aa05b2de39654271e68180726cf5fb
joande15 <31957175+joande15@users.noreply.github.com>
2017-11-02 09:48:27 +0100

    Recieves messages via interrupt

--- CAN_Buss_Recieve/CAN_Buss_Recieve.ino
+++ CAN_Buss_Recieve/CAN_Buss_Recieve.ino
@@ -1,18 +1,16 @@
 /*
-flytta variabelinitiering till init-funktion
 integrera setuprecieve med send
 skapa setupsend
 
 */
 
+//***CAN-Buss variabes**
 #include <mcp_can.h>
 #include <SPI.h>
-
 long unsigned int rxId;
 unsigned char len = 0;
 unsigned char rxBuf[8];
-char msgString[128];                        // Array to store serial string
-
+char msgString[128];                            // Array to store serial string
 #define CAN0_INT 2                              // Set INT to pin 2
 MCP_CAN CAN0(10);                               // Set CS to pin 10
 
@@ -21,15 +19,14 @@ void setup(){
   SetupCANBussRecieve();
 }
 
-void loop()
-{
-  
+void loop(){
+
   
    
 }
 
 void SetupCANBussRecieve(){
-  Serial.begin(115200);
+  Serial.begin(500000);
     
   // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
   if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK){
@@ -41,8 +38,8 @@ void SetupCANBussRecieve(){
   
   CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
   pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
-  attachInterrupt(digitalPinToInterrupt(CAN0_INT), ReadMessage, FALLING);
-  Serial.println("MCP2515 Library Receive Example...");
+  attachInterrupt(digitalPinToInterrupt(CAN0_INT), ReadMessage, FALLING);// interrupt activates when canbuss message can be read
+  
   
 }
 
